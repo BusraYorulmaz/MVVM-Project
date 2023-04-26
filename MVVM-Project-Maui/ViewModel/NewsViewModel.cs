061b62bd8fe550636d9b6aad80d68551620f45b0
Busra Yorulmaz <muhbusrayorulmaz@gmail.com>
2023-04-26 19:43:44 +0300

    26.04 commit

--- MVVM-Project-Maui/ViewModel/NewsViewModel.cs
+++ MVVM-Project-Maui/ViewModel/NewsViewModel.cs
@@ -1,42 +1,54 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks; 
 
 namespace MVVM_Project_Maui.ViewModel
 {
-    public class NewsViewModel :INotifyPropertyChanged
-    { 
-        public Command SignInCommand { get; set; }
-        private string userName;
+    public partial class NewsViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        string userName;
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public string UserName
+        [RelayCommand]
+        void Login()
         {
-            get { return userName; }
-            set { 
-                userName = value;
-                OnPropertyChanged("UserName");
-            }
+
         }
 
 
-        public NewsViewModel()
-        {
-            SignInCommand =  new Command(SignIn);
-        }
+        //INotifyPropertyChanged -> kullanıldığında kodlar aşağıdaki gibi olmalıdır.
+        //public Command SignInCommand { get; set; }
+        //private string userName;
 
-        private void SignIn(object obj)
-        {
-            var user = UserName; 
-        }
+        //public event PropertyChangedEventHandler PropertyChanged;
 
-        void OnPropertyChanged(string name)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
+        //public string UserName
+        //{
+        //    get { return userName; }
+        //    set { 
+        //        userName = value;
+        //        OnPropertyChanged("UserName");
+        //    }
+        //}
+
+        //public NewsViewModel()
+        //{
+        //    SignInCommand =  new Command(SignIn);
+        //}
+
+        //private void SignIn(object obj)
+        //{
+        //    var user = UserName; 
+        //}
+
+        //void OnPropertyChanged(string name)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        //}
     }
 }
